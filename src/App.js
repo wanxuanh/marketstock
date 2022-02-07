aadc6e3e414fc3b110bd1d5f98c5f408f5f76cf7
wanxuanh <wanxuan.ho@gmail.com>
2022-02-07 16:26:18 +0800

    stock api up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import StockInfo from "./StockInfo";
 
 function App() {
+  const [StockData, setStockData] = useState("AAPL");
+  const [StockTitle, setStockTitle] = useState("");
+
+  useEffect(() => {
+    const stockUrl = `https://api.twelvedata.com/stocks?symbol=${StockTitle}`;
+
+    const makeApiCall = async () => {
+      const res = await fetch(stockUrl);
+      const data = await res.json();
+      setStockData(data);
+    };
+
+    //makeApiCall();
+  }, []);
+
+  const handleSubmit = (title) => {
+    console.log("App - handleSubmit - title", title);
+
+    const url = `https://api.twelvedata.com/stocks?symbol=${title}`;
+
+    fetch(url) //call the url (GET REQUEST)
+      .then((res) => res.json()) //convert to json and pass
+      .then((res) => {
+        console.log("StockData", res.data);
+        setStockData(res.data);
+
+        // const title = data.Title;
+        // setStockTitle(title);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Live Market Stock App </div>
+      <Form handleSubmit={handleSubmit} />
+      <StockInfo stocks={StockData} />
+      <div>{JSON.stringify(StockData)}</div>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function Form(props) {
+  const [StockTitle, setStockTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    console.log("Form - handleSubmit - StockTitle", StockTitle);
+    e.preventDefault();
+    props.handleSubmit(StockTitle);
+    setStockTitle("");
+  };
+
+  const handleChange = (e) => {
+    console.log("handleChange clicked");
+    const title = e.target.value;
+    setStockTitle(title);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="StockTitle"></label>
+        <input
+          id="StockTitle"
+          type="text"
+          value={StockTitle}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Check Stock Info" />
+      </form>
+    </>
+  );
+}
+
+export default Form;
