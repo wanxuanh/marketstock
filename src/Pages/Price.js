444392ac91ffb302bb2a43718bcd86f0c276ad0c
wanxuanh <93863129+wanxuanh@users.noreply.github.com>
2022-03-10 23:32:41 +0800

    css update

--- src/Pages/Price.js
+++ src/Pages/Price.js
@@ -5,7 +5,7 @@ import { searchParams, useSearchParams } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 function Price(props) {
